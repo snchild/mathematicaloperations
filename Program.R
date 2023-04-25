4c874996ee321544f0b80b2c5c44e013223fc998
snchild <sierrachild@yahoo.com>
2023-04-25 10:54:54 -0600

    added design

--- Program.R
+++ Program.R
@@ -1,1 +1,38 @@
-print("Hello World")
+#goals:
+#   Use at least 5 different R datatypes in your program:
+#            character, numeric, integer, complex, logical
+#   Incorporate at least one loop that works with lists or arrays
+
+#stretch goal:
+#   Use the case of coding block
+
+add <- function(values) {
+    return() #sum of values
+}
+
+subtract <- function(values) {
+    return() #difference of values
+}
+
+multiply <- function(values) {
+    return() #product of values
+}
+
+divide <- function(values) {
+    return() #quotient of values
+}
+
+exponent <- function(values) {
+    return() #exponent of values
+}
+
+# prompt the user for pairs of numbers
+# check to see if any numbers are complex
+# stretch goal: prompt the user for a power function
+
+# create a dataframe
+# perform artithmetic
+# assign values to columns
+# make dataframe
+
+#stretch goal: perform derivative
