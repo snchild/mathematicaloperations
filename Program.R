44e0bea97d4afed6afd4284489dbc43486df2147
snchild <sierrachild@yahoo.com>
2023-04-25 11:16:07 -0600

    testing dataframe

--- Program.R
+++ Program.R
@@ -6,33 +6,40 @@
 #stretch goal:
 #   Use the case of coding block
 
-add <- function(values) {
-    return() #sum of values
+add <- function(val1, val2) {
+    return(val1 + val2)
 }
 
-subtract <- function(values) {
-    return() #difference of values
+sub <- function(val1, val2) {
+    return(val1 - val2)
 }
 
-multiply <- function(values) {
-    return() #product of values
+mult <- function(val1, val2) {
+    return(val1 * val2)
+    }
+
+div <- function(val1, val2) {
+    return(val1 / val2)
 }
 
-divide <- function(values) {
-    return() #quotient of values
-}
-
-exponent <- function(values) {
-    return() #exponent of values
+exp <- function(val1, val2) {
+    return(val1^val2)
 }
 
 # prompt the user for pairs of numbers
 # check to see if any numbers are complex
 # stretch goal: prompt the user for a power function
 
-# create a dataframe
-# perform artithmetic
-# assign values to columns
-# make dataframe
+my_data_frame <- data.frame(
+    # perform artithmetic
+    # assign values to columns
+    Addition = c(add(1, 2), add(2, 3), add(3, 4)),
+    Subtraction = c(sub(1, 2), sub(2, 3), sub(3, 4)),
+    Multiplication = c(mult(1, 2), mult(2, 3), mult(3, 4)),
+    Division = c(div(1, 2), div(2, 3), div(3, 4)),
+    Exponential = c(exp(1, 2), exp(2, 3), exp(3, 4))
+    )
+
+print(my_data_frame)
 
 #stretch goal: perform derivative
